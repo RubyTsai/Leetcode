88eff69cbaf4c273ed8051c9c9e6da119fd17f16
RubyTsai <1791843+RubyTsai@users.noreply.github.com>
2017-08-18 15:30:16 -0700

    Update 494_Target_Sum_dp.cpp
    我們假設數字前面為’+’的集合為 a，數字前面為’-‘的集合為 b，
    sum(a) 為選的正數的累加和，sum(b) 為選的負數的數字累加和
    則 S = sum(a) - sum(b)，
    S + sum(b) = sum a) ------兩邊再同時加上 sum(a)
    S + sum(b) +  sum(a) = 2 sum(a)
    sum(a) + sum(b) 則是整個數組的總和
    S + totalSum = 2 Sum(a) -------> newTarget / 2 = Sum(a)
    if newTarget is odd, then it is impossilbe to get S from nums
    
    因為 a 集合可為空集合，所以要考慮都不拿取的情況，因此 dp 的 row 要 + 1 (line 11)
    定義 dp[i][j] 前 i 個數夠成 j 有多少種方案

--- 494_Target_Sum_dp.cpp
+++ 494_Target_Sum_dp.cpp
@@ -4,17 +4,16 @@ public:
         int nsize = nums.size();
         int totalSum = 0;
         for (auto n: nums) totalSum += n;
-        if (S > totalSum || S < -totalSum) return 0;
-        int newTarget = S + totalSum;
-        if ((newTarget % 2) != 0) return 0;
-        
-        S = newTarget > 1;
+        if (S > totalSum || S < -totalSum ||
+            (S + totalSum) % 2 != 0) return 0;
+        S = (S + totalSum) >> 1; // new target
+
         vector<vector<int>> dp(nsize+1, vector<int>(S+1, 0));
-        
+        dp[0][0] = 1;
+            
         for (int i = 1; i <= nsize; i++) {
             for (int k = 0; k <= S; k++) {
-                if (k == nums[i-1]) dp[i][k] = dp[i-1][0] + 1;
-                if (k > nums[i-1]) dp[i][k] += dp[i-1][k-nums[i-1]]; // 取這個數
+                if (k >= nums[i-1]) dp[i][k] += dp[i-1][k-nums[i-1]]; // 取這個數
                 dp[i][k] += dp[i-1][k];
             }
         }
