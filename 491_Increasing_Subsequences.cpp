56819436f108627e2fd66316d0222e99d0f97ca5
RubyTsai <1791843+RubyTsai@users.noreply.github.com>
2017-08-19 19:55:47 -0700

    Create 491_Increasing_Subsequences.cpp

---
+++ 491_Increasing_Subsequences.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        unordered_set<int> sn;
+        vector<vector<vector<int>>
+    }
+private:
+    void helper(int i, unordered_set<int>& sn, vector<vector<vector<int>>& res) {
+        if (i >= nsize) return;
+        int num = nums[i];
+        
+        vector<int> newSeq;
+        if (sn.find(num) == sn.end()) { // new number
+            
+
+        }
+
+    }
+};
