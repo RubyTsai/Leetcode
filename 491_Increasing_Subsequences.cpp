2681cd1f04aea2404eb6da75c6ff9f378deac9bb
RubyTsai <1791843+RubyTsai@users.noreply.github.com>
2017-08-21 12:53:03 -0700

    Update 491_Increasing_Subsequences.cpp
    注意，不可用 unordered_set<vector<int>> ans;

--- 491_Increasing_Subsequences.cpp
+++ 491_Increasing_Subsequences.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> findSubsequences(vector<int>& nums) {
-        unordered_set<int> sn;
-        vector<vector<vector<int>>
-    }
-private:
-    void helper(int i, unordered_set<int>& sn, vector<vector<vector<int>>& res) {
-        if (i >= nsize) return;
-        int num = nums[i];
-        
-        vector<int> newSeq;
-        if (sn.find(num) == sn.end()) { // new number
-            
-
+    vector<vector<int>> findSubsequences(vector<int>& nums) {              
+        set<vector<int>> ans;      
+        for (int i = 0; i < nums.size(); i++) {
+            vector<vector<int>> seqs(ans.begin(), ans.end());
+            for (auto& seq: seqs) {
+                if (seq.back() <= nums[i]) {
+                    seq.push_back(nums[i]);
+                    ans.insert(seq);
+                }
+            }
+            ans.insert(vector<int>(1, nums[i]));
         }
-
+        
+        vector<vector<int>> res;
+        for (auto& seq: ans) {
+            if (seq.size() > 1) res.push_back(seq);
+        }
+        return res;
     }
 };
