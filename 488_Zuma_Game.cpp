772e9d1c8e5ca0919ee67c4b5543e447c007cb03
RubyTsai <1791843+RubyTsai@users.noreply.github.com>
2017-08-22 16:28:13 -0700

    Update 488_Zuma_Game.cpp

--- 488_Zuma_Game.cpp
+++ 488_Zuma_Game.cpp
@@ -1,25 +1,43 @@
 class Solution {
 public:
     int findMinStep(string board, string hand) {
-        bsize = board.size();
+        this->board = move(board);
+        bsize = this->board.size();
         bsquare = bsize*bsize;
         unordered_map<char, int> handMap;
         for (auto c: hand)
             handMap[c]++;
-        helper(0, bsize-1, 0, board, hand, dp);
+        int dp[20][20][20] = {bsquare};
+        int ret = helper(0, bsize-1, 0, handMap, dp);
+        return ret == bsquare? -1: ret;
     }
 private:
+    string board;
     int bsize, bsquare;
-    int helper (int start, int end, int same, string board, string hand, int dp[][][]) {
+    int helper (int start, int end, int same, unordered_map<char, int>& hand, int dp[20][20][20]) {
         if (start > end) return 0; // ?
         if (dp[start][end][same] != bsquare) return dp[start][end][same];
         
-        while (start < end && string[start] == string[start+1]) {
+        while (start < end && board[start] == board[start+1]) {
             start++;
             same++;
         }
-        int cost = 
-        if (same+1 >= 3) dp[start][end][same] = 0;
+        
+        unordered_map<char, int> newHand(hand);
+        //hand[board[start]] - (3-same-1) >= 0
+        if (same < 2 && hand[board[start]] >= (2-same)) {
+            hand[board[start]] -= 2-same; 
+            dp[start][end][same] = 2-same + helper(start+1, end, 0, hand, dp);
+            hand[board[start]] += 2-same; 
+        }
+        if (same > 1) dp[start][end][same] = helper(start+1, end, 0, hand, dp);
+        for (int i = start; i <= end; i++) {
+            if (board[i] == board[start]) {
+                dp[start][end][same] = min(dp[start][end][same], 
+                                           helper(start+1, i-1, 0, hand, dp) + 
+                                           helper(i, end, same+1, hand, dp));
+            }
+        }
+        return dp[start][end][same];       
     }
-
 };
