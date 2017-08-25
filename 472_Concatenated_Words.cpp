5a7996e11abb290be9a2ef70d9f4a3013f1b8554
RubyTsai <1791843+RubyTsai@users.noreply.github.com>
2017-08-25 14:42:14 -0700

    Update 472_Concatenated_Words.cpp

--- 472_Concatenated_Words.cpp
+++ 472_Concatenated_Words.cpp
@@ -3,6 +3,7 @@ bool cmpLen(const string& a, const string& b) {
 }
 class Solution {
 public:
+    // 此版本可 pass leetcode，但是若 words 有重複的字，則會有錯
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
         vector<string> ret;
         sort(words.begin(), words.end(), cmpLen);
@@ -35,3 +36,32 @@ private:
         return dp[w.size()];
     }
 };
+
+/* -------------------------
+
+若將 helper 改成這樣，就可以解決 input (words) 有重複的情況
+(關鍵在於不要把整個 w 丟進去 dict 裡面去找)
+ 
+    bool helper(string& w, unordered_set<string>& dict) {
+        if (dict.empty()) return false;
+        vector<bool> dp(w.size()+1, false);
+        dp[0] = true;
+        
+        for (int len = 1; len < w.size(); len++) {
+            for (int j = len-1; j >= 0; j--) {
+                if (dp[j] && dict.find(w.substr(j, len-j)) != dict.end()) {
+                    dp[len] = true;
+                    break;
+                }
+            }                
+        }
+        
+        for (int j = w.size() - 1; j > 0; j--) {
+            if (dp[j] && dict.find(w.substr(j, w.size()-j)) != dict.end()) {
+                dp[w.size()] = true;
+                break;
+            }
+        }               
+        return dp[w.size()];
+    }
+----------------------------*/
