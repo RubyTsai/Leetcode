67c300b617dcdfa47fa4bf77650ac8ccf6a8c60b
RubyTsai <1791843+RubyTsai@users.noreply.github.com>
2017-08-28 16:05:49 -0700

    Update 140_Word_Break_II.cpp
    c++ set count vs find: 
    https://stackoverflow.com/questions/1701067/how-to-check-that-an-element-is-in-a-stdset
    
    解題關鍵：
    若 helper 裡的原字串本身也可以在 dict 中找到，則直接放入 result 中

--- 140_Word_Break_II.cpp
+++ 140_Word_Break_II.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
     vector<string> wordBreak(string s, vector<string>& wordDict) {
-
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+        unordered_map<string, vector<string>> mdp;
+        return helper(s, dict, mdp);
     }
-    vector<string> helper (string s, unordered_set<string>& dict, ) {
+private:
+    vector<string> helper(string s, unordered_set<string>& dict, 
+                          unordered_map<string, vector<string>>& mdp) {
+        if (mdp.count(s)) return mdp[s]; //take from memory
+            
         vector<string> ret;
-        for (int len = 1; len <= s.size(); len++) {
-            string sub = s.substr(0, len);
-            if (dict.find(sub) != dict.end()) {
-                vector<string> append = helper(s.substr(len), );
-                ret.push_back(sub + " " + );
+        if (dict.count(s)) ret.push_back(s);//原字串本身也可以在 dict 中找到，則直接放入 result 中 
+        
+        for (int len = 1; len < s.size(); len++) {
+            string left = s.substr(0, len);
+            if (dict.count(left)) {
+                vector<string> suffixes= helper(s.substr(len), dict, mdp);
+                for (auto& suffix: suffixes)
+                    ret.push_back(move(left + " " + suffix));
             }
         }
+        mdp[s] = ret;
+        return ret;
     }
 };
