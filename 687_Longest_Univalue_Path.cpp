25560d64f3a34b16eb6f918f97ecd358f65b92db
RubyTsai <1791843+RubyTsai@users.noreply.github.com>
2017-10-01 15:37:49 -0700

    Update 687_Longest_Univalue_Path.cpp
    https://discuss.leetcode.com/topic/105653/c-dfs-with-explanation
    Perform DFS by recursively invoking helper for left and right subtrees. When coming back up the recursive stack, check to see if the root value is equal to the left and right subtree root value, if so, add one onto the max path value returned by the subtree to include the one additional path between the root and the subtree root.
    
    There are 3 possible use cases to consider when coming back up the recursive stack:
    
    Case 1: If the root value is equal to BOTH the left and right subtree root values, then set m to the maximum of itself compared to the "up-side-down-V shaped" path created by including the left and right subtrees max paths with the root ( l=l+1 and r=r+1 ). Return the max path value of the left and right subtrees return max(l,r) which is needed in case the next parent up the recursive stack is also the same value.
    
    Case 2: If the root value is NOT equal to EITHER the left INCLUSIVE-OR right subtree values, then set BOTH l=0 and r=0 and return 0 ( i.e. return max(0,0) ).
    
    Case 3: If the root value is equal to the left subtree root value XOR the right subtree root value, then either l=0 and r=r+1 XOR l=l+1 and r=0, return the maximum of the left and right subtree path values ( i.e. return max(l,0) XOR return max(0,r) ).
    
    Always check for a new max for each of these 3 use cases by adding the path value of the left subtree and right subtree. m=max(m,l+r). It is OK to add l and r together when checking for a new max for each of the 3 use cases above, since l and/or r are set to 0 when applicable ( i.e. for case 2: 0+0==0 and for case 3: l+0==l XOR 0+r==r ).

--- 687_Longest_Univalue_Path.cpp
+++ 687_Longest_Univalue_Path.cpp
@@ -1,19 +1,47 @@
+/*
+Given a binary tree, find the length of the longest path where each node in the path has the same value. 
+This path may or may not pass through the root.
+
+Note: The length of path between two nodes is represented by the number of edges between them.
+
+Ex1:                                Ex2:
+    Input:                              Input:
+              5                                 1
+             / \                               / \
+            4   5                             4   5
+           / \   \                           / \   \
+          1   1   5    Output: 2            4   4   5   Output: 2
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
     int longestUnivaluePath(TreeNode* root) {
-        if (root == NULL) return 0;
+        if (!root) return 0;
+        lp = 0;
+        helper(root);
+        return lp;
+    }
+private:
+    int lp;
+    int helper(TreeNode* root) {
         int val = root->val;
-        TreeNode* left = root->left, *right = root->right;
-        int lc = longestUnivaluePath(left);
-        int rc = longestUnivaluePath(right);
+        TreeNode* lc = root->left, *rc = root->right;
+      
+        int l = lc? helper(lc): 0;
+        int r = rc? helper(rc): 0;
         
-        if (left != NULL && left->val == val &&
-            right != NULL && right->val == val)
-            return lc+rc+2;
-        if (left != NULL && left->val == val)
-            return lc+1 > rc? lc+1: rc;
-        if (right != NULL && right->val == val)
-            return rc+1 > lc? rc+1: lc;
-        return lc > rc? lc: rc;
+        l = (lc && lc->val == val)? ++l: 0; 
+        r = (rc && rc->val == val)? ++r: 0;
+        
+        lp = max(lp, l+r);
+        return max(l, r);
     }
 };
