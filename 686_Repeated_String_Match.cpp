6467501b811def091a338d5af207e60aafca7ad8
RubyTsai <1791843+RubyTsai@users.noreply.github.com>
2017-09-30 20:27:50 -0700

    Create 686_Repeated_String_Match.cpp

---
+++ 686_Repeated_String_Match.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int repeatedStringMatch(string A, string B) {
+        int sa = A.size();
+        int sb = B.size();
+        
+        for (int i = 0; i <= sb; i++) {
+            if (A[0] == B[i]) {
+                        
+            }
+        }
+    }
+    
+    if (sb - i >= sa) {
+        if(B.find(A[a]) != std::string::npos) {
+            ret++;
+            if (sb - i == sa) break;
+            else i += sa;
+        }
+    }
+    else 
+}; 
