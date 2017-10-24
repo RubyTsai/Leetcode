b6594ef7364a82023397107e10ca41117ca3d402
RubyTsai <1791843+RubyTsai@users.noreply.github.com>
2017-10-24 12:44:31 -0700

    Create 713_Subarray_Product_Less_Than_K.cpp

---
+++ 713_Subarray_Product_Less_Than_K.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        int p = nums[0], ans = 0;
+        for (int l = 0, r = 0; r < nums.size(); ) {
+            if (p < k) {
+                ans += 1 + (r-l);
+                r++;
+                p *= nums[r];
+            }
+            else p = p/nums[l++];
+            
+            if (l > r) r = l;
+        }
+        return ans;
+    }
+};
+
+/*
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        vector<int> dp(nums);
+        int ans = 0, size = nums.size();
+        for (int i = 0; i < size; i++)
+            if (nums[i] < k) ans++;
+        
+        for (int i = 1; i < size; i++)
+            for (int j = 0, x = i+j; j < size-i; j++, x++) {
+                if (dp[x] > k) continue;
+                dp[x] = dp[x] * nums[j];
+                if (dp[x] < k) ans++;
+            }
+        return ans;
+    }
+};
+*/
