02e277ac4e3c8b3051a30c16add32135f245244b
RubyTsai <1791843+RubyTsai@users.noreply.github.com>
2017-11-10 18:21:37 -0800

    Create 238_Product_of_Array_Except_Self.cpp

---
+++ 238_Product_of_Array_Except_Self.cpp
@@ -0,0 +1,37 @@
+// Space: O(2n)
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int size = nums.size();
+        vector<int> before(size, 1);
+        vector<int> after(size, 1);
+        for (int i = 1; i < size; i++) 
+            before[i] = before[i-1] * nums[i-1];
+        for (int i = size-2; i >=0; i--)
+            after[i] = after[i+1] * nums[i+1];
+        
+        vector<int> ans(size);
+        for (int i = 0; i < size; i++)
+            ans[i] = before[i] * after[i];
+        return ans;
+    }
+};
+
+// Space: O(n)
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int size = nums.size();
+        vector<int> ans(size, 1);
+        for (int i = 1; i < size; i++) 
+            ans[i] = ans[i-1] * nums[i-1];
+        
+        int after = 1;
+        for (int i = size-1; i >=0; i--) {
+            ans[i] *= after;
+            after *= nums[i];
+        }
+        
+        return ans;
+    }
+};
